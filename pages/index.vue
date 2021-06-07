22c1fd79f8b73242868a19af800bd920de623df9
Sam Matuba <jcmatuba@up.edu.ph>
2021-06-07 12:23:05 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,64 @@
 <template>
-  <main>    
-    <PageHomeHero />
-    <PageHomeOurSolutions />
-    <PageHomeOurProjects />
-    <PageHomeAbout />
-    <LazyPageHomeOurPartners />                   
-  <!--
-    <h2>Posts</h2>
-    <div v-for="post in posts" :key="post.id" class="post">
-      <h3>
-        <a :href="`blog/${post.slug}`">{{ post.title.rendered }}</a>
-      </h3>
-      <div v-html="post.excerpt.rendered" />
-      <a :href="`blog/${post.slug}`" class="readmore">Read more ⟶</a>
+  <div class="flex flex-col w-screen h-screen bg-white">
+    <!--Top-->
+    <div class="flex flex-1 absolute top-0 w-full justify-center p-4">
+      <img
+        class="m-0 transform sm:scale-50 mx-auto"
+        src="~/assets/main/orig_logo.png"
+      />
     </div>
-    -->
-  </main>
+    <div class="flex-col m-auto p-4 space-y-5 place-items-center">
+      <div class="flex justify-center max-h-16">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          class="animate-spin-slow"
+        >
+          <path
+            style="fill: #fcd34d"
+            fill-rule="evenodd"
+            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
+      <h1 class="font-mono text-center text-4xl">https://excell.energy/</h1>
+      <h1
+        class="
+          font-sans font-semibold
+          text-center text-4xl
+          subpixel-antialiased
+          bg-clip-text
+          text-transparent
+          bg-gradient-to-b
+          from-green-lime
+          to-green
+        "
+      >
+        RISING SOON
+      </h1>
+      <div class="flex justify-center items-baseline">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          viewBox="0 0 18 18"
+          fill="currentColor"
+        >
+          <path
+            d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"
+          />
+        </svg>
+        <h2 class="font-semibold font-sans text-center text-2xl">
+          +63 922 882 0336
+        </h2>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
+
+<style>
+</style>
