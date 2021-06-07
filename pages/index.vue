580fd56d65b55e7d7db3ab27d40b5a37c0926ee0
Sam Matuba <jcmatuba@up.edu.ph>
2021-06-07 13:57:16 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -49,7 +49,7 @@
           />
         </svg>
         <a class="font-semibold font-sans text-center text-2xl"
-        href="tel:63 922 882 0336">
+        href="tel:+639228820336">
           +63 922 882 0336
         </a>
       </div>
